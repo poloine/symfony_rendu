35dbdd1e637f28bbc9c61111709847bac8f2282f
Antoine B <aberthillot@myges.fr>
2025-01-31 23:41:44 +0100

    Added others features

--- symfony_tp_m1_iw/src/Controller/AdministrationController.php
+++ symfony_tp_m1_iw/src/Controller/AdministrationController.php
@@ -2,17 +2,72 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class AdministrationController extends AbstractController
 {
     #[Route('/administration', name: 'app_administration')]
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
         return $this->render('administration/index.html.twig', [
-            'controller_name' => 'AdministrationController',
+            'users' => $userRepository->findAll(),
         ]);
+    }
+
+
+    #[Route('/administration/new', name: 'app_administration_new_user', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_administration');
+        }
+
+        return $this->render('administration/user/new.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/administration/user/{id}', name: 'app_administration_user_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_administration');
+        }
+
+        return $this->render('administration/user/edit.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/administration/user/{id}', name: 'app_administration_user_delete', methods: ['POST'])]
+    public function delete(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $user->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($user);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_administration');
     }
 }
